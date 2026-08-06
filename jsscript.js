5c2b15484b3bc1c78c95e72b51492a4ddd318682
MurphyZTM191211 <wwahmaung@gmail.com>
2026-08-06 10:30:12 +0630

    Update jsscript.js

--- jsscript.js
+++ jsscript.js
@@ -33,6 +33,12 @@ const products = [
     name: "Mechanical Keyboard",
     price: "$79.99",
     image: "https://th.bing.com/th/id/OIP.TX5oCdj9Q0-VvHX8JiSX8QHaEK?w=326&h=180&c=7&r=0&o=7&pid=1.7&rm=3"
+  },
+  {
+    id: 5,
+    name: "Smart TV",
+    prize: "$7999.99",
+    image: "https://th.bing.com/th/id/OIP.kUH80e8ldsGrPXfmHFQuXQHaEK?w=318&h=180&c=7&r=0&o=7&pid=1.7&rm=3".
   }
 ];
 
