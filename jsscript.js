20aabd95c27b69a85794ca331a74ff4118b155d0
MurphyZTM191211 <wwahmaung@gmail.com>
2026-08-06 10:34:51 +0630

    Update jsscript.js

--- jsscript.js
+++ jsscript.js
@@ -35,7 +35,7 @@ const products = [
     image: "https://th.bing.com/th/id/OIP.TX5oCdj9Q0-VvHX8JiSX8QHaEK?w=326&h=180&c=7&r=0&o=7&pid=1.7&rm=3"
   },
   {
-    id: 5,
+    id: 6,
     name: "Smart TV",
     prize: "$7999.99",
     image: "https://th.bing.com/th/id/OIP.kUH80e8ldsGrPXfmHFQuXQHaEK?w=318&h=180&c=7&r=0&o=7&pid=1.7&rm=3".
