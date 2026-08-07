2cbb8f9d48ca20fe0841c966167f906d057333f5
MurphyZTM191211 <wwahmaung@gmail.com>
2026-08-07 19:50:24 +0630

    Add streaming webcam product to jsscript.js

--- jsscript.js
+++ jsscript.js
@@ -49,6 +49,12 @@ const products = [
     price: "$8999.99",
     image: "https://th.bing.com/th/id/OIP.XL68FVcM11kOIq1RU3Cn3AHaHY?w=188&h=187&c=7&r=0&o=7&pid=1.7&rm=3"
   },
+  {
+    id: 9,
+    name: "streaming webcam",
+    price: "$199.99",
+    image: "https://th.bing.com/th/id/OIP.uSahOc3jisd9yOmQwICt5AAAAA?w=206&h=206&c=7&r=0&o=7&pid=1.7&rm=3"
+  },
 ];
 
 // 2. DOM Elements
